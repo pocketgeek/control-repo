ed1c0fc8eff8dcb792f06277ea6173b8b5d06f18
Curtis Edge <pocket_geek@pgnet.us>
2021-03-18 15:42:50 -0600

    base profile take 2

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,5 +1,28 @@
-class profile::base {
+class profile::base (
 
-  #the base profile should include component modules that will be on all nodes
+  Array[String] $sysadmins,
+  String $admingroup,
+  String $sysadminplayground,
+  Array[String] $stdpackages,
+) {
+
+  #sysadmin accounts
+  group{ $admingroup:
+    ensure => present,
+  }
+
+  #Add all the admin users specified in hiera and make sure they are in group $admingroup
+  $sysadmins.each |String $account| {
+    user{ $account:
+      ensure => present,
+      groups => $admingroup,
+    }
+  }
+
+  #Make sure they have Log on as a service rights.
+  local_security_policy { "Log on as a service":
+    ensure       => present,
+    policy_value => $admingroup,
+  }
 
 }
