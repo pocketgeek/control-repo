7fefc21272afc5968cb2ff097b44dc3a728cf254
Curtis Edge <pocket_geek@pgnet.us>
2021-03-18 15:52:22 -0600

    hiera error checking could be better

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -7,22 +7,22 @@ class profile::base (
 ) {
 
   #sysadmin accounts
-  group{ $admingroup:
+  group{ $sysadmingroup:
     ensure => present,
   }
 
-  #Add all the admin users specified in hiera and make sure they are in group $admingroup
+  #Add all the admin users specified in hiera and make sure they are in group $sysadmingroup
   $sysadmins.each |String $account| {
     user{ $account:
       ensure => present,
-      groups => $admingroup,
+      groups => $sysadmingroup,
     }
   }
 
   #Make sure they have Log on as a service rights.
   local_security_policy { "Log on as a service":
     ensure       => present,
-    policy_value => $admingroup,
+    policy_value => $sysadmingroup,
   }
 
 }
